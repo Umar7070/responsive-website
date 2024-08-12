49e8e606fe07d372e40d2279949e121899e6d50e
umar7070 <mohdumar707080@gmail.com>
2024-08-12 11:19:01 +0530

    navbar responsive

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let menu= document.querySelector("#menuList");
-menu.addEventListener("click",()=>{
-console.log("click  here")
-})
+
+
+let list = document.querySelector(".list");
+let menu =document.querySelector(".menu")
+let cross = document.querySelector(".cross")
+
+function humburgMenu (){
+  list.classList.toggle("active");
+  menu.style.visibility= "hidden";
+  cross.style.visibility="visible";
+}
+
+function crossMenu(){
+  list.classList.remove("active");
+  menu.style.visibility="visible"
+  cross.style.visibility="hidden";
+}
